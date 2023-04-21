9ddc987fee36e9045660cae3a154f32816cd839b
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-04-21 21:31:12 +0900

    01_02_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -2,18 +2,100 @@
 #include "TextureManager.h"
 #include <cassert>
 
+#include "ImGuiManager.h"
+
+#include "PrimitiveDrawer.h"
+
+#include "AxisIndicator.h"
+
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete sprite_; 
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("sample.png");
+
+	// スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("sample.png");
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	// ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	// サウンドデータの読み込み
+	soundDateHandle_ = audio_->LoadWave("fanfare.wav");
+
+	// 音声再生
+	audio_->PlayWave(soundDateHandle_);
+
+	// 音声再生
+	voiceHandle_ = audio_->PlayWave(soundDateHandle_, true);
+
+	// ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
+	// デバッグカメラの生成
+	debugCamera_ = new DebugCamera(1280, 720);
+
+	// 軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	// 軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+
+	// スプライトの今の座標を取得
+	Vector2 positoin = sprite_->GetPosition();
+	// 座標を { 2, 1 }移動
+	positoin.x += 2.0f;
+	positoin.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(positoin);
+
+	// スペースキー押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+	// 音声停止
+		audio_->StopWave(voiceHandle_);
+	}
+
+	// デバッグテキストの表示
+	ImGui::Text("kamata Tarou %d %d %d", 2050, 12, 31);
+
+	ImGui::Begin("Debug1");
+	ImGui::Text("Kamata Tarou %d %d %d", 2050, 12, 31);
+	ImGui::End();
+
+	ImGui::Begin("Debgu1");
+	// float3入力ボックス
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	// float3スライダー
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+
+	ImGui::ShowDemoWindow();
+
+	// デバッグカメラの更新
+	debugCamera_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -27,6 +109,10 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
+
+	// ラインを描画する
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
@@ -41,6 +127,10 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+
+	
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
