5b380d90bd13406dbe952a693fbc5101c39ce422
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-07-21 14:06:39 +0900

    mousukosi

--- GameObject/RailCamera/RailCamera.cpp
+++ GameObject/RailCamera/RailCamera.cpp
@@ -8,8 +8,8 @@ void RailCamera::Initialize() {
 	// ワールドトランスフォームの初期設定
 	worldTransform_.scale_ = {1.0f, 1.0f, 1.0f};
 	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
-	worldTransform_.translation_ = {0.0f, 0.0f, -10.0f};
-
+	worldTransform_.translation_ = {0.0f, 0.0f, -50.0f};
+	worldTransform_.Initialize();
 
 	// ビュープロジェクションの初期化
 	viewProjection_.Initialize();
@@ -24,25 +24,21 @@ void RailCamera::Update() {
 
 	// ワールドトランスフォームの加算
 	Vector3 move = {0.0f, 0.0f, 0.0f};
-	const float kRailCameraSpeed = -0.3f;
-	move.z -= kRailCameraSpeed;
+	//const float kRailCameraSpeed = -0.3f;
+	//move.z -= kRailCameraSpeed;
 	worldTransform_.translation_ = Add(worldTransform_.translation_, move);
 
 	Vector3 rotate = {0.0f, 0.0f, 0.0f};
 	worldTransform_.rotation_ = Add(worldTransform_.rotation_, rotate);
 
 
-	//// ワールドトランスフォームのワールド行列再計算
-	//worldTransform_.UpdateMatrix();
+	// ワールドトランスフォームのワールド行列再計算
+	worldTransform_.UpdateMatrix();
 
 	
 	// アフィン変換行列
 	worldTransform_.matWorld_ = MakeAffineMatrix(
 	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
-
-	
-	// 行列を定数バッファに転送
-	worldTransform_.TransferMatrix();
 
 
 	// カメラオブジェクトのワールド行列からビュー行列を計算する
@@ -53,8 +49,8 @@ void RailCamera::Update() {
 	ImGui::Begin("RailCamera");
 
 	// スライダーでtranslationとratationを表示
-	ImGui::SliderFloat3("translation", &worldTransform_.translation_.x, -30.0f, 30.0f);
-	ImGui::SliderFloat3("rotation", &worldTransform_.rotation_.x, -30.0f, 30.0f);
+	ImGui::SliderFloat3("translation", &worldTransform_.translation_.x, -50.0f, 50.0f);
+	ImGui::SliderFloat3("rotation", &worldTransform_.rotation_.x, -50.0f, 50.0f);
 
 	ImGui::End();
 }
